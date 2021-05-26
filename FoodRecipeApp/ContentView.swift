7b922cd3ae3064c746d19622103eed802c46a74e
JuraevDev <tezkormedia@gmail.com>
2021-05-26 15:21:37 +0300

    Initial Commit

--- FoodRecipeApp/ContentView.swift
+++ FoodRecipeApp/ContentView.swift
@@ -13,28 +13,20 @@ struct ContentView: View {
         
         TabView(selection: $selected ){
             HomeView().tabItem {
-                HStack{
                     Image(systemName:  self.selected == 0 ? "house.fill" : "house")
                     Text("Home")
-                }
             }.tag(0)
             CategoryView().tabItem {
-                HStack{
                     Image(systemName: self.selected == 1 ? "circle.grid.2x2.fill" : "circle.grid.2x2")
                     Text("Category")
-                }
             }.tag(1)
             SavedView().tabItem {
-                HStack{
                     Image(systemName: self.selected == 2 ? "bookmark.fill" : "bookmark")
                     Text("Saved")
-                }
             }.tag(2)
             ProfileView().tabItem {
-                HStack{
                     Image(systemName: self.selected == 3 ? "person.fill" : "person")
                     Text("Profile")
-                }
             }.tag(3)
         }
         .accentColor(Color(#colorLiteral(red: 0.3691508174, green: 0.6315197945, blue: 0.5072926283, alpha: 1)))
